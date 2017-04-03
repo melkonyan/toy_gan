5b504d271399f4e86aa9e7df312058e310448622
Alex Melkonyan <sasha.melkonyan@gmail.com>
2017-04-03 17:36:21 +0200

    Rewrite code using OOP, implement WGAN

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def gen_data(num_of_samples):
     return samples
 
 
-def gen_noise(num_of_samples):
-    return np.linspace(0, 1, num_of_samples) + np.random.random(num_of_samples) * 0.01
+def gen_noise(num_of_samples, range=8):
+    return np.linspace(-range, range, num_of_samples) + np.random.random(num_of_samples) * 0.01
 
 
 def linear(input, output_size, scope='linear'):
@@ -40,27 +40,42 @@ def discriminator(data, hidden_layer = 10):
     return h3
 
 
-def gan(batch_size=1000, test_size=10000):
-    N = batch_size
-    with tf.variable_scope('G'):
-        noise = tf.placeholder(tf.float32, [None, 1], "z")
-        gen = generator(noise)
-    with tf.variable_scope('D') as scope:
-        data = tf.placeholder(tf.float32, [None, 1], "d")
-        dis_fake = discriminator(gen)
-        scope.reuse_variables()
-        dis_real = discriminator(data)
-    loss_gen = tf.reduce_mean(-tf.log(dis_fake))
-    loss_dis = tf.reduce_mean(-tf.log(dis_real)) + tf.reduce_mean(-tf.log(1-dis_fake))
-    optimizer = tf.train.RMSPropOptimizer(0.001)
-    vars = tf.trainable_variables()
-    d_params = [v for v in vars if v.name.startswith('D/')]
-    g_params = [v for v in vars if v.name.startswith('G/')]
-    opt_dis = optimizer.minimize(loss_dis, var_list=d_params)
-    opt_gen = optimizer.minimize(loss_gen, var_list=g_params)
-    # data_batch = gen_data(batch_size)
-    # noise_batch = gen_noise(batch_size)
-    with tf.Session() as session:
+class GAN(object):
+
+    def __init__(self):
+        self._create_models()
+
+    def _create_models(self):
+        with tf.variable_scope('G'):
+            self.noise = tf.placeholder(tf.float32, [None, 1], "z")
+            self.gen = generator(self.noise)
+        with tf.variable_scope('D') as scope:
+            self.data = tf.placeholder(tf.float32, [None, 1], "d")
+            self.dis_fake = discriminator(self.gen)
+            scope.reuse_variables()
+            self.dis_real = discriminator(self.data)
+
+    def _loss_gen(self):
+        return tf.reduce_mean(-tf.log(self.dis_fake))
+
+    def _loss_dis(self):
+        return tf.reduce_mean(-tf.log(self.dis_real)) + tf.reduce_mean(-tf.log(1-self.dis_fake))
+
+    def _dis_post_update(self, session):
+        pass
+
+    def train(self, batch_size=1000, test_size=10000):
+        loss_gen = self._loss_gen()
+        loss_dis = self._loss_dis()
+        optimizer = tf.train.RMSPropOptimizer(0.01)
+        vars = tf.trainable_variables()
+        self.d_params = [v for v in vars if v.name.startswith('D/')]
+        self.g_params = [v for v in vars if v.name.startswith('G/')]
+        opt_dis = optimizer.minimize(loss_dis, var_list=self.d_params)
+        opt_gen = optimizer.minimize(loss_gen, var_list=self.g_params)
+        #data_batch = gen_data(batch_size)
+        #noise_batch = gen_noise(batch_size)
+        session = tf.Session()
         init = tf.global_variables_initializer()
         session.run(init)
         steps_num = 1000
@@ -68,11 +83,12 @@ def gan(batch_size=1000, test_size=10000):
             data_batch = gen_data(batch_size)
             noise_batch = gen_noise(batch_size)
             feed_dict = {
-                data: np.reshape(data_batch, (batch_size,1)),
-                noise: np.reshape(noise_batch, (batch_size,1))
+                self.data: np.reshape(data_batch, (batch_size,1)),
+                self.noise: np.reshape(noise_batch, (batch_size,1))
             }
             session.run(opt_gen, feed_dict)
             session.run(opt_dis, feed_dict)
+            self._dis_post_update(session)
             if step % (steps_num / 10) == 0:
                 l_g, l_d = session.run([loss_gen, loss_dis], feed_dict)
                 print("Gen loss: %g, dis loss: %g" % (l_g, l_d))
@@ -80,19 +96,32 @@ def gan(batch_size=1000, test_size=10000):
         dec_size = 1000
         t_data = np.linspace(-10, 10, dec_size)
         feed_dict = {
-            data: np.reshape(t_data, (dec_size, 1)),
-            noise: np.reshape(t_noise, (test_size, 1))
+            self.data: np.reshape(t_data, (dec_size, 1)),
+            self.noise: np.reshape(t_noise, (test_size, 1))
         }
-        dec_boundary = session.run(dis_real, feed_dict)
+        dec_boundary = session.run(self.dis_real, feed_dict)
         print("Test generator loss: %g" % session.run(loss_gen, feed_dict))
         print("Test discriminator loss: %g" % session.run(loss_dis, feed_dict))
-        return session.run(gen, feed_dict), np.transpose(np.column_stack((t_data, dec_boundary)))
+        return session.run(self.gen, feed_dict), np.transpose(np.column_stack((t_data, dec_boundary)))
+
+
+class WGAN(GAN):
+
+    def _loss_gen(self):
+        return -tf.reduce_mean(self.dis_fake)
+
+    def _loss_dis(self):
+        return -tf.reduce_mean(self.dis_real) + tf.reduce_mean(self.dis_fake)
+
+    def _dis_post_update(self, session):
+        self.d_params = [tf.clip_by_value(param, -3, 3) for param in self.d_params]
+
 
 real_data = gen_data(10000)
-fake_data, dec_boundary = gan()
+fake_data, dec_boundary = WGAN().train()
 plt.hist(real_data, 100, histtype='step', normed=True, label='Real data')
 plt.hist(fake_data, 100, histtype='step', normed=True, label='Fake data')
 plt.plot(dec_boundary[0], dec_boundary[1], 'b', label='Decision boundary')
 plt.legend()
-#plt.show()
-plt.savefig("images/mini_batch.png")
+plt.show()
+#plt.savefig("images/batch.png")
